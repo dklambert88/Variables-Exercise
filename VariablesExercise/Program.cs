736052ab7489584ed8dd69cbb367a9479347ca22
David Lambert <davidlambert+adobe@adobe.com>
2025-08-19 22:16:33 -0400

    First commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,6 +4,15 @@
     {
         static void Main(string[] args)
         {
+            string catName = "Milo";
+            int catAge = 8;
+            char sampleLetter = 'A';
+            bool isComplete = true;
+            double sampleDouble = 3.14159;
+            decimal sampleDecimal = 99.95m;
+
+            Console.WriteLine($"My cat {catName} is {catAge} years old.");
+
             Console.WriteLine("Hello, World!");
         }
     }
